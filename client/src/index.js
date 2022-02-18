2d8adcca2eb6fcd3f1c49864a376e81c1a82338f
nikolapivac <npivac00@fesb.hr>
2022-02-18 19:40:48 +0100

    Installed dependecies for frontend

---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+// used for connecting react application to index.html file
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
