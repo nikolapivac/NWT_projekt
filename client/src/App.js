dabe3ba6b103bc6e158a7e34fffe545fb5685462
nikolapivac <npivac00@fesb.hr>
2022-02-19 11:29:47 +0100

    Added some simple styles

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import logo from './images/logo.png';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
+import useStyles from './styles'
 
 const App = () => {
+    const classes = useStyles();
     return (
         <Container maxwidth="lg">
-            <AppBar position="static" color="inherit">
-                <Typography variant="h2" align="center">TITLE</Typography>
-                <img src={logo} alt="logo" height="60" />
+            <AppBar className={classes.appBar} position="static" color="inherit">
+                <Typography className={classes.heading} variant="h2" align="center">PhotoPost</Typography>
+                <img className={classes.image} src={logo} alt="logo" height="60" />
             </AppBar>
             <Grow in>
                 <Container>
