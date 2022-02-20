b07e6287454e7db16599398551326879f4b03a7c
nikolapivac <npivac00@fesb.hr>
2022-02-20 18:35:48 +0100

    Bumped Form to front on mobile devices, added some constants and environmental variables

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,15 +1,16 @@
 //handling the logic for each possible action type
+import { FETCH_ALL, CREATE, UPDATE, LIKE, DELETE } from '../constants/actionTypes';
 
 export default (posts = [], action) => {
     switch (action.type) {
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
+        case LIKE:
             return posts.map((post) => post._id === action.payload._id ? action.payload : post);
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
-        case 'CREATE':
+        case CREATE:
             // spreading the existing posts and adding a new one (stored in the payload of the action)
             return [...posts, action.payload];
         default:
