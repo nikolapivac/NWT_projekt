86a05e441e1c20b549ad245fc8a7e8cdc4a4a212
nikolapivac <npivac00@fesb.hr>
2022-02-20 11:59:31 +0100

    Worked on Posts and Post components and installed Font Awesome

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Post from './Post/Post';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core'
 
 import useStyles from './styles'
     
@@ -9,11 +10,16 @@ const Posts = () => {
     const posts = useSelector((state) => state.posts);
     const classes = useStyles();
     return (
-        <>
-        <h1>POSTS</h1>
-        <Post />
-        <Post />
-        </>
+        // if there is no posts, show CircularProgress; if there are some posts, show them
+        !posts.length ? <CircularProgress /> : (
+            <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+                {posts.map((post) => (
+                    <Grid key={post._id} item xs={12} sm={6}>
+                        <Post post={post}/>
+                    </Grid>
+                ))}
+            </Grid>
+        )
     )
 }
 
