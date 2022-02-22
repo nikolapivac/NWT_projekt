349b17baf6c7c659f112cba7b2a17fc9fe5ec8ab
nikolapivac <npivac00@fesb.hr>
2022-02-22 11:23:30 +0100

    Made changes to Toolbar, made Navbar, Home and Auth components

--- server/index.js
+++ server/index.js
@@ -11,7 +11,6 @@ const app = express();
 dotenv.config();
 
 
-
 // setting up the body parser so we can properly send requests
 app.use(bodyParser.json({ limit: "30mb", extended: true}))
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}))
