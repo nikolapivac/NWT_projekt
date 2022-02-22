73844f48826e092d017940150434714c8489c088
nikolapivac <npivac00@fesb.hr>
2022-02-22 14:01:41 +0100

    Added "Sign Up/Sign In" form

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, Button, Paper, Grid, Typography, Container, TextField } from "@material-ui/core";
+import useStyles from "./styles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLock } from "@fortawesome/free-solid-svg-icons";
+import Input from './Input';
 
 const Auth = () => {
-    const state = null;
+  const classes = useStyles();
+  //useState for showing/hiding password
+  const [showPassword, setShowPassword] = useState(false);
+  //useState for switching between Sign In and Sign Up
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  //show/hide password toggle
+  const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword);
+  //Sing Up - Sign In toggle
+  const switchMode = () => {
+      setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+      handleShowPassword(false);
+    }
+
+  const handleSubmit = () => {
+
+  }
+
+  const handleChange = () => {
+
+  }
+
+  
+
   return (
-    <div>
-      AUTH
-    </div>
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <FontAwesomeIcon icon={faLock} />
+        </Avatar>
+        <Typography variant="h5">{isSignUp ? 'Sign Up' : 'Sign In'}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+              {
+                isSignUp && (
+                  <>
+                    <Input name="firstName" label="First Name" handleChange={handleChange} autoFocus half />
+                    <Input name="lastName" label="Last Name" handleChange={handleChange} half />
+                  </>
+                )
+              }
+              <Input name="email" label="Email address" handleChange={handleChange} type="email" /> 
+              <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? "text" : "password"} handleShowPassword={handleShowPassword} /> 
+              { isSignUp && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password"/>}
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" className={classes.submit}>
+            {isSignUp ? "Sign Up" : "Sign In"}
+          </Button>
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button className={classes.button} onClick={switchMode}>
+                {isSignUp ? "Already have an account? Sign In" : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   )
 }
 
---
+++ client/src/components/Auth/Input.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@material-ui/core';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+
+const input = ({ name, handleChange, label, half, autoFocus, type, handleShowPassword }) => {
+  return (
+    //if the half prop is true, we want half the size (for Name and Last Name)
+    <Grid item xs={12} sm={half ? 6 : 12}>
+        <TextField 
+            name={name}
+            onChange={handleChange}
+            variant="outlined"
+            required
+            fullWidth
+            label={label}
+            autoFocus={autoFocus}
+            type={type}
+            InputProps={name === 'password' && {
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleShowPassword}>
+                            {type === "password" ? <FontAwesomeIcon icon={faEye} /> : <FontAwesomeIcon icon={faEyeSlash} />}
+                        </IconButton>
+                    </InputAdornment>
+                )
+            }}
+        
+        />
+    </Grid>
+  )
+}
+
+export default input
